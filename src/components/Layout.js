338c8c41e2013395fa535ade2b6d37b822de3eeb
schmidt-aaron <aschmidt808@gmail.com>
2019-05-24 11:43:12 -0700

    sass + basic styles

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,7 @@
 import React, { Children } from "react"
 import Header from "./Header"
 import Footer from "./Footer"
+import "../styles/index.scss"
 
 export default function Layout({ children }) {
   return (
